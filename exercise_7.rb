90aa5e3e93141db25cac04fe478bbbb77157d0c7
Bence Kiss <kiss.bence.balint@gmail.com>
2017-01-17 14:21:15 -0500

    Bonus task: calculating the total number of students across all cohorts.

--- exercise_7.rb
+++ exercise_7.rb
@@ -10,19 +10,27 @@ students = {
   cohort2: 42,
   cohort3: 22
 }
-
+#add a new cohort with 43 guys/girls in it
 students[:cohort4] = 43
 
 puts students.keys
 
 printhash(students)
-
+#increment cohorts' size by 5%
 students.each do |k, v|
   students[k] = v * 1.05
 end
 
 printhash(students)
 
+#deleting the second cohort
 students.delete(:cohort2)
 
 printhash(students)
+
+#Bonus, count the total number of students
+total = 0.0
+students.each do |k, v|
+  total += v
+end
+puts "The total number of students across all cohorts is #{total.to_i}."
